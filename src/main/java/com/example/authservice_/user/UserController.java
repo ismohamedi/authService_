8f7f56c04c2537f862fd7c5eda92b9a4b9baa4e9
Ismaili Lichinga <ismaili.lichinga@tra.go.tz>
2023-03-31 13:04:43 +0300

    clean unused imports

--- src/main/java/com/example/authservice_/user/UserController.java
+++ src/main/java/com/example/authservice_/user/UserController.java
@@ -5,8 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 

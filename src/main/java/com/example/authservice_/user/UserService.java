a30aac71351ab78856f8b1c604f980407e988cb2
Ismaili Lichinga <ismaili.lichinga@tra.go.tz>
2023-04-05 12:03:25 +0300

    remove unused inports

--- src/main/java/com/example/authservice_/user/UserService.java
+++ src/main/java/com/example/authservice_/user/UserService.java
@@ -2,8 +2,6 @@ package com.example.authservice_.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 import java.util.Optional;
 
